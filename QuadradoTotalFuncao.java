841c67672020ae09ea07eb4074a558ad5488c21a
kaiquerebonato487-svg <kaique.rebonato487@al.unieduk.com.br>
2026-08-17 19:24:12 -0300

    Update QuadradoTotalFuncao.java

--- QuadradoTotalFuncao.java
+++ QuadradoTotalFuncao.java
@@ -1,9 +1,8 @@
 import java.util.Arrays;
 import java.util.List;
 
-// sem loop, sem variável sendo alterada: map eleva ao quadrado,
-// reduce soma tudo
-public class SomaQuadradosFuncional {
+
+public class QuadradoTotalFuncao {
 
     public static void main(String[] args) {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4);
@@ -12,8 +11,6 @@ public class SomaQuadradosFuncional {
                 .map(n -> n * n)
                 .reduce(0, Integer::sum);
 
-        // dava pra fazer com mapToInt(...).sum() também, fica igual
-
-        System.out.println("Soma dos quadrados: " + resultado);
+        System.out.println("Quadrado Total: " + resultado);
     }
 }
