f630444cc226d1b3e24a64da6925c98740afcb62
kaiquerebonato487-svg <kaique.rebonato487@al.unieduk.com.br>
2026-08-17 19:24:50 -0300

    Update QuadradosTotal.java

--- QuadradosTotal.java
+++ QuadradosTotal.java
@@ -1,15 +1,13 @@
 import java.util.List;
 
-// classe que guarda a lista e sabe calcular a soma dos quadrados dela
-public class SomaQuadrados {
+public class QuadradoTotal {
 
     private final List<Integer> numeros;
 
-    public SomaQuadrados(List<Integer> numeros) {
+    public QuadradoTotal(List<Integer> numeros) {
         this.numeros = numeros;
     }
 
-    // método privado, ninguém de fora precisa saber que ele existe
     private int quadrado(int n) {
         return n * n;
     }
