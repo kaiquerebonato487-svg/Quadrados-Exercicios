adaafd593e342003df313f7c8d94f1d10cf67b0e
kaiquerebonato487-svg <kaique.rebonato487@al.unieduk.com.br>
2026-08-24 09:21:53 -0300

    Update QuadradosTotal.pl

--- QuadradosTotal.pl
+++ QuadradosTotal.pl
@@ -1,17 +1,11 @@
 :- encoding(utf8).
 
-% quadrado total de uma lista, sem loop, só caso-base + recursão
 
-% caso base: lista vazia resulta em 0
 quadrado_total([], 0).
 
-% caso recursivo: quadrado da cabeça + resultado da cauda
 quadrado_total([Cabeca|Cauda], Total) :-
     quadrado_total(Cauda, TotalCauda),
     Quadrado is Cabeca * Cabeca,
     Total is Quadrado + TotalCauda.
 
-% testado no SWI-Prolog:
-%   ?- consult('quadrado_total.pl').
-%   ?- quadrado_total([1,2,3,4], R).
-%   R = 30.
+
